b1951a73951357d54f641a2f05fb7023b2a41a5c
Sanju Kamat <sanjukamat123@gmail.com>
2021-11-18 10:11:03 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,194 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.447460500003114,
+        "pitch": 0.04749387118560122,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6124749495383881,
+          "pitch": 0.08242776753446357,
+          "rotation": 4.71238898038469,
+          "target": "3-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.24546684741906333,
+          "pitch": -0.16636636560648732,
+          "title": "SWIMMING POOL",
+          "text": "Swimming Pool Rules<div>&nbsp;</div><div>- No outside Food &amp; Drinks near&nbsp; &nbsp; &nbsp; &nbsp;the Pool.</div><div><br></div><div>- Children to use the pool under&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;adult supervision .</div><div><br></div><div>- Appropriate clothing mandatory.</div><div>&nbsp;</div><div>-&nbsp; Management&nbsp; is not responsible</div><div>&nbsp; &nbsp;for any unfortunate incidents by&nbsp; &nbsp; &nbsp; &nbsp; the&nbsp; pool.&nbsp;</div><div><br></div><div>&nbsp;<br></div><div><div><br></div><div><br></div></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.0596849093414793,
+        "pitch": -0.005945470051369028,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.211033098319124,
+          "pitch": 0.032675987678633334,
+          "rotation": 0,
+          "target": "2-3"
+        },
+        {
+          "yaw": 0.03442081883411596,
+          "pitch": 0.09265178609351565,
+          "rotation": 7.853981633974483,
+          "target": "3-4"
+        },
+        {
+          "yaw": 2.0781773751346773,
+          "pitch": -0.008473259333625194,
+          "rotation": 1.5707963267948966,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.980111246568633,
+        "pitch": -0.008585943833081089,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9581727857384656,
+          "pitch": 0.06055571114502456,
+          "rotation": 7.853981633974483,
+          "target": "3-4"
+        },
+        {
+          "yaw": 2.074638816424806,
+          "pitch": 0.03287144401682163,
+          "rotation": 4.71238898038469,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-4",
+      "name": "4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.5163921743679296,
+        "pitch": -0.07205513910990646,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.47296082826101227,
+          "pitch": 0.007424787327520832,
+          "rotation": 1.5707963267948966,
+          "target": "0-1"
+        },
+        {
+          "yaw": 1.5110197019031189,
+          "pitch": 0.011798069311305781,
+          "rotation": 0,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Swimming Pool ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
